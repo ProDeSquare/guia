f300c18af11d1c9903fbf7ae3e4f87dee0a98243
Hamza Mughal <hamza@prodesquare.com>
2021-12-26 01:55:09 +0500

    navigation and footer component

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Guia</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
+    <script src="./assets/js/require.min.js"></script>
+    <script>
+        requirejs.config({
+            baseUrl: '.'
+        });
+    </script>
+
+    <!-- Dashboard Core -->
+    <link href="./assets/css/dashboard.css" rel="stylesheet" />
+    <script src="./assets/js/dashboard.js"></script>
+    <!-- c3.js Charts Plugin -->
+    <link href="./assets/plugins/charts-c3/plugin.css" rel="stylesheet" />
+    <script src="./assets/plugins/charts-c3/plugin.js"></script>
+    <!-- Input Mask Plugin -->
+    <script src="./assets/plugins/input-mask/plugin.js"></script>
+</head>
+<body>
+    <div class="page">
+        <div class="page-main">
+            @include('partials.navigation')
+            @yield('content')
+
+            @include('partials.footer')
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,102 @@
+<div class="header py-4">
+    <div class="container">
+        <div class="d-flex">
+            <a class="header-brand" href="./index.html">
+            <img src="{{ asset("assets/images/logo.png") }}" class="header-brand-img" alt="riphah logo">
+            </a>
+            <div class="d-flex order-lg-2 ml-auto">
+                <div class="dropdown">
+                    <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
+                    <span class="avatar" style="background-image: url(https://www.gravatar.com/avatar/322eeecb2780eeaffb3f541bcae38b27?s=250)"></span>
+                    <span class="ml-2 d-none d-lg-block">
+                    <span class="text-default">Hamza Mughal</span>
+                    <small class="text-muted d-block mt-1">Administrator</small>
+                    </span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                        <a class="dropdown-item" href="#">
+                        <i class="dropdown-icon fe fe-user"></i> Profile
+                        </a>
+                        <a class="dropdown-item" href="#">
+                        <i class="dropdown-icon fe fe-settings"></i> Settings
+                        </a>
+                        <a class="dropdown-item" href="#">
+                        <span class="float-right"><span class="badge badge-primary">6</span></span>
+                        <i class="dropdown-icon fe fe-mail"></i> Inbox
+                        </a>
+                        <a class="dropdown-item" href="#">
+                        <i class="dropdown-icon fe fe-send"></i> Message
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">
+                        <i class="dropdown-icon fe fe-help-circle"></i> Need help?
+                        </a>
+                        <a class="dropdown-item" href="#">
+                        <i class="dropdown-icon fe fe-log-out"></i> Sign out
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse" data-target="#headerMenuCollapse">
+            <span class="header-toggler-icon"></span>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-3 ml-auto">
+                <form class="input-icon my-3 my-lg-0">
+                    <input type="search" class="form-control header-search" placeholder="Search&hellip;" tabindex="1">
+                    <div class="input-icon-addon">
+                        <i class="fe fe-search"></i>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg order-lg-first">
+                <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+                    <li class="nav-item">
+                        <a href="./index.html" class="nav-link active"><i class="fe fe-home"></i> Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-box"></i> Interface</a>
+                        <div class="dropdown-menu dropdown-menu-arrow">
+                            <a href="./cards.html" class="dropdown-item ">Cards design</a>
+                            <a href="./charts.html" class="dropdown-item ">Charts</a>
+                            <a href="./pricing-cards.html" class="dropdown-item ">Pricing cards</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-calendar"></i> Components</a>
+                        <div class="dropdown-menu dropdown-menu-arrow">
+                            <a href="./maps.html" class="dropdown-item ">Maps</a>
+                            <a href="./icons.html" class="dropdown-item ">Icons</a>
+                            <a href="./store.html" class="dropdown-item ">Store</a>
+                            <a href="./blog.html" class="dropdown-item ">Blog</a>
+                            <a href="./carousel.html" class="dropdown-item ">Carousel</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-file"></i> Pages</a>
+                        <div class="dropdown-menu dropdown-menu-arrow">
+                            <a href="./profile.html" class="dropdown-item ">Profile</a>
+                            <a href="./login.html" class="dropdown-item ">Login</a>
+                            <a href="./register.html" class="dropdown-item ">Register</a>
+                            <a href="./forgot-password.html" class="dropdown-item ">Forgot password</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="./form-elements.html" class="nav-link"><i class="fe fe-check-square"></i> Forms</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./gallery.html" class="nav-link"><i class="fe fe-image"></i> Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./docs/index.html" class="nav-link"><i class="fe fe-file-text"></i> Documentation</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
