cd712ee7271e7173d95223d3920fd16f36c07ee0
Hamza Mughal <hamza@prodesquare.com>
2021-12-28 18:36:17 +0500

    changed mod index page

--- resources/views/mods/index.blade.php
+++ resources/views/mods/index.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
     <div class="container">
-        <a href="{{ route('mod.add') }}">Add a moderator</a>
+        <div class="page-header">
+            <div class="page-title">
+                You're logged in as a moderator
+            </div>
+        </div>
+
+        <div class="page-body">
+            <div>Actions: </div>
+
+            <ul>
+                <li><a href="#">Add a Student</a></li>
+                <li><a href="#">Add a teacher</a></li>
+            </ul>
+        </div>
     </div>
 @endsection
