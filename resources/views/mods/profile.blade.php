7153dd2c712dd1f032b93b086f301729c3c84598
Hamza Mughal <hamza@prodesquare.com>
2022-01-15 20:29:52 +0500

    mod profile view now contains added teachers and students

--- resources/views/mods/profile.blade.php
+++ resources/views/mods/profile.blade.php
@@ -24,47 +24,73 @@
             </div>
 
             <div class="col-lg-8">
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="card-title">
-                            Teachers ({{ $mod->teachers()->count() }})
-                        </h3>
-
-                        @if ($mod->teachers()->count())
-                            <ul>
+                {{-- teachers list --}}
+                <div class="card" bis_skin_checked="1">
+                    <div class="card-header" bis_skin_checked="1">
+                        <h3 class="card-title">Teachers ({{ $mod->teachers()->count() }})</h3>
+                    </div>
+                    <div class="card-body o-auto" style="height: 15rem" bis_skin_checked="1">
+                        <ul class="list-unstyled list-separated">
+                            @if ($mod->teachers()->count())
                                 @foreach ($mod->teachers()->latest()->take(5)->get() as $teacher)
-                                    <li>
-                                        <a href="{{ route('teacher.profile', ['teacher' => $teacher->id]) }}">
-                                            {{ $teacher->name }}
-                                        </a>
+                                    <li class="list-separated-item">
+                                        <div class="row align-items-center" bis_skin_checked="1">
+                                            <div class="col-auto" bis_skin_checked="1">
+                                                <span class="avatar avatar-md d-block" style="background-image: url({{ $teacher->avatar() }})"></span>
+                                            </div>
+                                            <div class="col" bis_skin_checked="1">
+                                                <div bis_skin_checked="1">
+                                                    <a href="{{ route('teacher.profile', ['teacher' => $teacher->id]) }}" class="text-inherit">{{ $teacher->name }}</a>
+                                                </div>
+                                                <small class="d-block item-except text-sm text-muted h-1x">
+                                                    {{ $teacher->email }}
+                                                </small>
+                                            </div>
+                                        </div>
                                     </li>
                                 @endforeach
-                            </ul>
-                        @else
-                            <p>{{ $mod->name }} hasn't added any teachers yet.</p>
-                        @endif
+                            @else
+                                <small class="d-block item-except text-sm text-muted h-1x">
+                                    {{ $mod->name }} hasn't added any teachers yet.
+                                </small>
+                            @endif
+                        </ul>
                     </div>
                 </div>
 
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="card-title">
-                            Students ({{ $mod->students()->count() }})
-                        </h3>
-
-                        @if ($mod->students()->count())
-                            <ul>
+                {{-- students list --}}
+                <div class="card" bis_skin_checked="1">
+                    <div class="card-header" bis_skin_checked="1">
+                        <h3 class="card-title">Students ({{ $mod->students()->count() }})</h3>
+                    </div>
+                    <div class="card-body o-auto" style="height: 15rem" bis_skin_checked="1">
+                        <ul class="list-unstyled list-separated">
+                            @if ($mod->students()->count())
                                 @foreach ($mod->students()->latest()->take(5)->get() as $student)
-                                    <li>
-                                        <a href="{{ route('student.profile', ['student' => $student->id]) }}">
-                                            {{ $student->name }}
-                                        </a>
+                                    <li class="list-separated-item">
+                                        <div class="row align-items-center" bis_skin_checked="1">
+                                            <div class="col-auto" bis_skin_checked="1">
+                                                <span class="avatar avatar-md d-block" style="background-image: url({{ $student->avatar() }})"></span>
+                                            </div>
+                                            <div class="col" bis_skin_checked="1">
+                                                <div bis_skin_checked="1">
+                                                    <a href="{{ route('student.profile', ['student' => $student->id]) }}" class="text-inherit">{{ $student->name }}</a>
+                                                </div>
+                                                @if ($student->email)
+                                                    <small class="d-block item-except text-sm text-muted h-1x">
+                                                        {{ $student->email }}
+                                                    </small>
+                                                @endif
+                                            </div>
+                                        </div>
                                     </li>
                                 @endforeach
-                            </ul>
-                        @else
-                            <p>{{ $mod->name }} hasn't added any students yet.</p>
-                        @endif
+                            @else
+                                <small class="d-block item-except text-sm text-muted h-1x">
+                                    {{ $mod->name }} hasn't added any students yet.
+                                </small>
+                            @endif
+                        </ul>
                     </div>
                 </div>
             </div>
