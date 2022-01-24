6feac1c121af8b01ecc111424ad77140794f0e12
Hamza Mughal <hamza@prodesquare.com>
2022-01-24 19:42:41 +0500

    admin & moderator dashboard

--- resources/views/mods/index.blade.php
+++ resources/views/mods/index.blade.php
@@ -9,7 +9,91 @@
         </div>
 
         <div class="page-body">
-            
+            <div class="row row-cards">
+                <div class="col-6 col-sm-4 col-lg-2">
+                    <div class="card">
+                        <div class="card-body p-3 text-center">
+                            <div class="h1 mt-4 mb-0">
+                                {{ \App\Models\Admin::count() }}
+                            </div>
+
+                            <div class="text-muted mb-4">
+                                Admins
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-sm-4 col-lg-2">
+                    <div class="card">
+                        <div class="card-body p-3 text-center">
+                            <div class="h1 mt-4 mb-0">
+                                {{ \App\Models\Mod::count() }}
+                            </div>
+
+                            <div class="text-muted mb-4">
+                                Moderators
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-sm-4 col-lg-2">
+                    <div class="card">
+                        <div class="card-body p-3 text-center">
+                            <div class="h1 mt-4 mb-0">
+                                {{ \App\Models\Teacher::count() }}
+                            </div>
+
+                            <div class="text-muted mb-4">
+                                Teachers
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-sm-4 col-lg-2">
+                    <div class="card">
+                        <div class="card-body p-3 text-center">
+                            <div class="h1 mt-4 mb-0">
+                                {{ \App\Models\Student::count() }}
+                            </div>
+
+                            <div class="text-muted mb-4">
+                                Students
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-sm-4 col-lg-2">
+                    <div class="card">
+                        <div class="card-body p-3 text-center">
+                            <div class="h1 mt-4 mb-0">
+                                {{ \App\Models\Group::count() }}
+                            </div>
+
+                            <div class="text-muted mb-4">
+                                FYP Groups
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-sm-4 col-lg-2">
+                    <div class="card">
+                        <div class="card-body p-3 text-center">
+                            <div class="h1 mt-4 mb-0">
+                                {{ \App\Models\Project::where('status', 1)->count() }}
+                            </div>
+
+                            <div class="text-muted mb-4">
+                                Projects
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
