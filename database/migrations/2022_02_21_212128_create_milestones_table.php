b41e9d92025ddca8c4f5ce755704c8b8268a71c8
Hamza Mughal <hamza@prodesquare.com>
2022-02-22 02:25:57 +0500

    milestones migration

---
+++ database/migrations/2022_02_21_212128_create_milestones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMilestonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('milestones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id');
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('is_completed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('milestones');
+    }
+}
