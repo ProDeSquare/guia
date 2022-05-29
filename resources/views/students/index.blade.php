ea1c2cf1667e31c2e9faaf2c666a2a98960efdaf
Hamza Mughal <hamza@prodesquare.com>
2022-05-30 00:47:35 +0500

    showing assignments in card

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,21 +3,37 @@
 @section('content')
     <div class="container">
         <div class="page-header">
-            <h3 class="page-title">Student Dashboard</h3>
+            <h3 class="page-title">
+                {{ Auth::guard()->user()->name }}'s Dashboard
+            </h3>
         </div>
 
         <div class="page-body">
             @if (Auth::guard()->user()->isGrouped())
                 @if (Auth::guard()->user()->assignments()->count())
-                    <ul>
-                        @foreach (Auth::guard()->user()->assignments()->latest()->get() as $assignment)
-                            <li>
-                                <a href="{{ route('assignment.view', [$assignment->project()->first()->id, $assignment->milestone()->first()->id, $assignment->id]) }}">
-                                    {!! $assignment->is_completed ? '<del>' . $assignment->title . '</del>' : $assignment->title !!}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        Recent Assignments
+                                    </h3>
+                                </div>
+        
+                                <div class="card-body">
+                                    <ul>
+                                        @foreach (Auth::guard()->user()->assignments()->latest()->get() as $assignment)
+                                            <li>
+                                                <a href="{{ route('assignment.view', [$assignment->project()->first()->id, $assignment->milestone()->first()->id, $assignment->id]) }}">
+                                                    {!! $assignment->is_completed ? '<del>' . $assignment->title . '</del>' : $assignment->title !!}
+                                                </a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @elseif (Auth::guard()->user()->mainGroup()->projects()->count())
                     @if (Auth::guard()->user()->mainGroup()->acceptedProject())
                         <p>Nothing was yet assigned to you by your supervisor.</p>
