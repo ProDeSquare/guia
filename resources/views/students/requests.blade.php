eeeaeab3de4f5fa2d15298be1d3905cb65df2306
Hamza Mughal <hamza@prodesquare.com>
2022-06-01 19:53:41 +0500

    fix #22

--- resources/views/students/requests.blade.php
+++ resources/views/students/requests.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="page-header">
-            <h3 class="page-title">Pending Group Requests</div>
+            <h3 class="page-title">Pending Group Requests</h3>
         </div>
 
         <div class="page-body">
