95cb6f78199f5c41776557c8388e487c1d04d520
Hamza Mughal <hamza@prodesquare.com>
2022-06-12 02:47:22 +0500

    getting the projects based on year and without the year

--- app/Http/Controllers/History/ProjectsController.php
+++ app/Http/Controllers/History/ProjectsController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\History;
 
+use App\Models\Project;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    //
+    protected $projects;
+
+    public function __invoke($year = null)
+    {
+        $this->setProjects($year);
+
+        if (!$this->projects->count()) abort(404);
+
+        dd($this->projects);
+    }
+
+    protected function setProjects($year)
+    {
+        if ($year) {
+            $this->projects = Project::whereYear('created_at', $year)->simplePaginate(25);
+            return;
+        }
+
+        $this->projects = Project::simplePaginate(25);
+    }
 }
