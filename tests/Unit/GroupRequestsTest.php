7b5560b20b6bd60a24aebd7af80049be6fda83bb
Hamza Mughal <hamza@prodesquare.com>
2022-06-19 02:55:04 +0500

    projects unit testing

--- tests/Unit/GroupRequestsTest.php
+++ tests/Unit/GroupRequestsTest.php
@@ -48,19 +48,4 @@ class GroupRequestsTest extends TestCase
 
         $response->assertRedirect('/group/1/view/projects');
     }
-
-    public function test_students_can_update_project()
-    {
-        $this->actingAs(Student::first(), 'student');
-
-        $response = $this->patch('/group/update/project/1', [
-            'title' => 'The best project',
-            'description' => 'lorem ipsum dolor',
-            'technologies' => 'AI, ML, Docker',
-            'github_repo' => 'https://github.com/prodesquare/final-year-project',
-            'link' => 'https://fyp.prodesquare.com',
-        ]);
-
-        $response->assertRedirect('/group/view/project/1');
-    }
 }
