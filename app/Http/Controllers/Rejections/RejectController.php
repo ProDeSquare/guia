863f9587bd87335c7b45e9d4bbd944435d25b4ae
Hamza Mughal <hamza@prodesquare.com>
2022-10-07 03:25:31 +0500

    notifications while rejecting supervision requests

--- app/Http/Controllers/Rejections/RejectController.php
+++ app/Http/Controllers/Rejections/RejectController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers\Rejections;
 
+use App\Models\Group;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Modules\NotificationModules\NotificationService;
 
 class RejectController extends Controller
 {
+    protected $notificationService;
+
     public function __construct ()
     {
-        return $this->middleware('auth:teacher');
+        $this->middleware('auth:teacher');
+        $this->notificationService = new NotificationService();
     }
 
     public function __invoke (Project $project, Request $request)
@@ -31,6 +36,26 @@ class RejectController extends Controller
 
         $group->supervisorRequests()->where('teacher_id', Auth::guard()->id())->delete();
 
+        $this->sendNotifications($group);
+
         return redirect()->back();
+    }
+
+    protected function sendNotifications (Group $group)
+    {
+        $FcmTokens = [];
+
+        foreach ($group->members()->get() as $member) $FcmTokens[] = $member->student()->first()->device_token;
+
+        $notification = new Request();
+        $notification->setMethod('POST');
+        $notification->request->add([
+            'FcmToken' => $FcmTokens,
+            'title' => 'Supervision Request',
+            'description' => Auth::guard()->user()->name . " has rejected your supervision request.",
+            'icon' => Auth::guard()->user()->avatar(),
+        ]);
+
+        $this->notificationService->send($notification);
     }
 }
