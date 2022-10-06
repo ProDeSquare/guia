b1a2dd84d9fe3fe3869ba660fb9ac33992770656
Hamza Mughal <hamza@prodesquare.com>
2022-10-07 02:37:00 +0500

    notifications after accepting group request

--- app/Http/Controllers/Student/AcceptRequestController.php
+++ app/Http/Controllers/Student/AcceptRequestController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers\Student;
 
 use Auth;
+use App\Models\Group;
 use App\Models\GroupMember;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Modules\NotificationModules\NotificationService;
 
 class AcceptRequestController extends Controller
 {
+    protected $notificationService;
+
     public function __construct ()
     {
-        return $this->middleware('auth:student');
+        $this->middleware('auth:student');
+        $this->notificationService = new NotificationService();
     }
 
     public function __invoke (GroupMember $request)
@@ -22,6 +28,8 @@ class AcceptRequestController extends Controller
             return redirect()->back()->withFailure('Group has already reached max members!');
         }
 
+        $this->sendNotifications($request->group()->first());
+
         $request->update([
             'accepted' => 1,
         ]);
@@ -29,5 +37,23 @@ class AcceptRequestController extends Controller
         Auth::guard()->user()->groupRequests()->delete();
 
         return redirect()->route('home')->withSuccess('Successfully accepted group request!');
+    }
+
+    protected function sendNotifications (Group $group)
+    {
+        $FcmTokens = [];
+
+        foreach ($group->members()->get() as $member) $FcmTokens[] = $member->student()->first()->device_token;
+
+        $notification = new Request();
+        $notification->setMethod('POST');
+        $notification->request->add([
+            'FcmToken' => $FcmTokens,
+            'title' => 'Group Request',
+            'description' => Auth::guard()->user()->name . " has accepted your group request.",
+            'icon' => Auth::guard()->user()->avatar(),
+        ]);
+
+        $this->notificationService->send($notification);
     }
 }
