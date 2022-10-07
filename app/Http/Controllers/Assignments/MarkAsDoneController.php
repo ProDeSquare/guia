deb92d5b4f04013d9cf431871da1511974413fe1
Hamza Mughal <hamza@prodesquare.com>
2022-10-08 01:18:06 +0500

    notifications while student submits an assignment

--- app/Http/Controllers/Assignments/MarkAsDoneController.php
+++ app/Http/Controllers/Assignments/MarkAsDoneController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers\Assignments;
 
 use App\Models\Project;
+use App\Models\Teacher;
 use App\Models\Milestone;
 use App\Models\Assignment;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\AssignmentCompletionRequest;
+use App\Modules\NotificationModules\NotificationService;
 
 class MarkAsDoneController extends Controller
 {
+    protected $notificationService;
+
     public function __construct()
     {
         $this->middleware('auth:student');
+        $this->notificationService = new NotificationService();
     }
 
     public function __invoke(AssignmentCompletionRequest $request, Project $project, Milestone $milestone, Assignment $assignment)
@@ -27,6 +33,22 @@ class MarkAsDoneController extends Controller
             'is_completed' => true,
         ]);
 
+        $this->sendNotfication(Teacher::find($project->group()->first()->supervisor()->first()->teacher_id));
+
         return redirect()->route('assignment.view', [$project, $milestone, $assignment]);
+    }
+
+    protected function sendNotfication (Teacher $teacher)
+    {
+        $notification = new Request();
+        $notification->setMethod('POST');
+        $notification->request->add([
+            'FcmToken' => [$teacher->device_token],
+            'title' => 'New Submission',
+            'description' => Auth::guard()->user()->name . " has marked the assginment as complete",
+            'icon' => Auth::guard()->user()->avatar(),
+        ]);
+
+        $this->notificationService->send($notification);
     }
 }
