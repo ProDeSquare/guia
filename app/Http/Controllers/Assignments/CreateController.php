51cedf221942e22a877b7e95f321a2762cbd31e9
Hamza Mughal <hamza@prodesquare.com>
2022-10-08 01:03:38 +0500

    notifications while accepting supervisor assigns an assignment

--- app/Http/Controllers/Assignments/CreateController.php
+++ app/Http/Controllers/Assignments/CreateController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers\Assignments;
 
 use App\Models\Project;
+use App\Models\Student;
 use App\Models\Milestone;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\AssignmentsRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\AssignmentsRequest;
+use App\Modules\NotificationModules\NotificationService;
 
 class CreateController extends Controller
 {
+    protected $notificationService;
+
     public function __construct()
     {
         $this->middleware('auth:teacher');
+        $this->notificationService = new NotificationService();
     }
 
     public function add(AssignmentsRequest $request, Project $project, Milestone $milestone)
@@ -22,6 +28,8 @@ class CreateController extends Controller
         if (!$project->group()->first()->members()->where('student_id', $request->student_id)->count()) abort(404);
 
         $milestone->assignments()->firstOrCreate($request->validated());
+
+        $this->sendNotfication(Student::find($request->student_id));
 
         return redirect()->route('milestone.view', [$project->id, $milestone->id]);
     }
@@ -35,5 +43,19 @@ class CreateController extends Controller
             'members' => $project->group()->first()->members()->get(),
             'milestone' => $milestone,
         ]);
+    }
+
+    protected function sendNotfication (Student $student)
+    {
+        $notification = new Request();
+        $notification->setMethod('POST');
+        $notification->request->add([
+            'FcmToken' => [$student->device_token],
+            'title' => 'New Assignment',
+            'description' => Auth::guard()->user()->name . " has assigned you a new assignment",
+            'icon' => Auth::guard()->user()->avatar(),
+        ]);
+
+        $this->notificationService->send($notification);
     }
 }
