0b6c852fb73acab91ef58e2f2fc1682644bf6201
Hamza Mughal <hamza@prodesquare.com>
2022-10-09 04:31:40 +0500

    cleaned up the supervision `RejectController`

--- app/Http/Controllers/Rejections/RejectController.php
+++ app/Http/Controllers/Rejections/RejectController.php
@@ -7,15 +7,18 @@ use App\Models\Project;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Modules\GroupModules\GroupService;
 use App\Modules\NotificationModules\NotificationService;
 
 class RejectController extends Controller
 {
+    protected $groupService;
     protected $notificationService;
 
     public function __construct ()
     {
         $this->middleware('auth:teacher');
+        $this->groupService = new GroupService();
         $this->notificationService = new NotificationService();
     }
 
@@ -36,26 +39,14 @@ class RejectController extends Controller
 
         $group->supervisorRequests()->where('teacher_id', Auth::guard()->id())->delete();
 
-        $this->sendNotifications($group);
+        $this->notificationService->send($this->notificationService->prepare(
+            $this->groupService->getDeviceTokens($group),
+            'Request Rejected',
+            Auth::guard()->user()->name . " has rejected your supervision request.",
+            Auth::guard()->user()->avatar(),
+            route('teacher.profile', Auth::guard()->id())
+        ));
 
         return redirect()->back();
-    }
-
-    protected function sendNotifications (Group $group)
-    {
-        $FcmTokens = [];
-
-        foreach ($group->members()->get() as $member) $FcmTokens[] = $member->student()->first()->device_token;
-
-        $notification = new Request();
-        $notification->setMethod('POST');
-        $notification->request->add([
-            'FcmToken' => $FcmTokens,
-            'title' => 'Supervision Request',
-            'description' => Auth::guard()->user()->name . " has rejected your supervision request.",
-            'icon' => Auth::guard()->user()->avatar(),
-        ]);
-
-        $this->notificationService->send($notification);
     }
 }
