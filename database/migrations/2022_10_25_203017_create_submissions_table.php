980321e523c4438a68016af29e671d14fa4cb5e9
Hamza Mughal <hamza@prodesquare.com>
2022-10-26 01:32:29 +0500

    submissions migration

---
+++ database/migrations/2022_10_25_203017_create_submissions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assignment_id');
+            $table->text('submission')->nullable();
+            $table->string('github_commit_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submissions');
+    }
+}
