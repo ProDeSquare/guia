72334534caa933c42aa2722bda78e2cdd621bace
Hamza Mughal <hamza@prodesquare.com>
2022-10-26 02:30:20 +0500

    assignment can now be submitted

--- app/Http/Controllers/Submission/SubmitController.php
+++ app/Http/Controllers/Submission/SubmitController.php
@@ -6,6 +6,7 @@ use App\Models\Project;
 use App\Models\Milestone;
 use App\Models\Assignment;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\AssignmentCompletionRequest;
 
 class SubmitController extends Controller
@@ -22,6 +23,25 @@ class SubmitController extends Controller
                                 Assignment $assignment
                             )
     {
-        dd($request);
+        if ($assignment->is_completed) abort(403);
+
+        $project->milestones()->findOrFail($milestone->id);
+        $milestone->assignments()->findOrFail($assignment->id);
+
+        if (
+            (Auth::guard()->user()->getGuardType() === 'student' &&
+            !Auth::guard()->user()->isAssigned($assignment)) ||
+            (Auth::guard()->user()->getGuardType() === 'teacher' &&
+            !$project->group()->first()->isSupervisedBy(Auth::guard()->id()))
+        ) abort(403);
+
+        $assignment->submissions()->create([
+            'user_id' => Auth::guard()->id(),
+            'guard' => Auth::guard()->user()->getGuardType(),
+            'submission' => $request->submission,
+            'github_commit_link' => $request->github_commit_link,
+        ]);
+
+        return redirect()->route('assignment.view', [$project, $milestone, $assignment]);
     }
 }
