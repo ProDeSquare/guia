664f3560bdde90772cabd64dae04796316ea0b31
Hamza Mughal <hamza@prodesquare.com>
2022-10-26 03:05:21 +0500

    showing submissions

--- resources/views/assignments/index.blade.php
+++ resources/views/assignments/index.blade.php
@@ -4,6 +4,10 @@
     | (Assignment) {{ $assignment->title }}
 @endsection
 
+@php
+    $supervisor = $project->group()->first()->supervisor()->first()->supervisor()->first();
+@endphp
+
 @section('content')
     <div class="container">
         <div class="page-header">
@@ -79,12 +83,7 @@
                         </div>
                     </div>
 
-                    @if ($assignment->submissions()->count())
-                        @foreach ($assignment->submissions()->simplePaginate(12) as $submission)
-                            {{ $submission }}
-                        @endforeach
-                    @endif
-
+                    {{-- Student submit assignment form --}}
                     @if (
                         Auth::guard('student')->check() &&
                         Auth::guard()->user()->isAssigned($assignment) &&
@@ -128,6 +127,112 @@
                                     </div>
                                 </form>
                             </div>
+                        </div>
+                    @endif
+
+                    {{-- Teacher reply form --}}
+                    @if (
+                        Auth::guard('teacher')->check() &&
+                        $supervisor->id === Auth::guard()->id() &&
+                        !$assignment->is_completed
+                    )
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Add Comment</h3>
+                            </div>
+                            
+                            <div class="card-body">
+                                <form action="{{ route('add.assignment.submission', [$project, $milestone, $assignment]) }}" method="post">
+                                    @csrf
+
+                                    <div class="form-group">
+                                        <label for="submission" class="form-label">Comment/Message <span class="text-red">*</span></label>
+
+                                        <textarea name="submission" id="submission" class="form-control" rows="7" placeholder="Reply to the assignment" required>{{ old('submission') }}</textarea>
+
+                                        <small class="form-hint">
+                                            This is field accepts markdown
+                                        </small>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="github_commit_link" class="form-label">GitHub Issue Link</label>
+        
+                                        <input type="text" class="form-control @error('github_commit_link') is-invalid @enderror" id="github_commit_link" placeholder="Issue link from git repository" name="github_commit_link" value="{{ old('github_commit_link') }}" />
+
+                                        <small class="form-hint">
+                                            This is field is optional but if you've created an issue on git then paste the link in the field and submit.
+                                        </small>
+
+                                        @error('github_commit_link')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group mb-0">
+                                        <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    @endif
+
+                    {{-- Submissions --}}
+                    @if ($assignment->submissions()->count())
+                        @foreach ($assignment->submissions()->simplePaginate(12) as $submission)
+                            <div class="card card-body mb-5">
+                                <div class="row">
+                                    <div class="col-lg-1 mb-4">
+                                        @if ($submission->guard === 'student')
+                                            <div
+                                                class="avatar d-block"
+                                                style="background-image: url({{ $assignment->student()->first()->avatar() }})"
+                                            ></div>
+                                        @else
+                                            <div
+                                                class="avatar d-block"
+                                                style="background-image: url({{ $supervisor->avatar() }})"
+                                            ></div>
+                                        @endif
+                                    </div>
+
+                                    <div class="col-lg">
+                                        <p class="my-1">
+                                            @if ($submission->guard === 'student')
+                                                <a href="{{ route('student.profile', $assignment->student()->first()->id) }}">
+                                                    {{ $assignment->student()->first()->name }}
+                                                </a>
+                                            @else
+                                                <a href="{{ route('teacher.profile', $supervisor) }}">
+                                                    {{ $supervisor->name }}
+                                                </a>
+                                            @endif
+
+                                            <span class="text-muted">• {{ ucwords($submission->guard) }}</span>
+                                        </p>
+
+                                        <div class="mt-4">
+                                            {{ Markdown::parse($submission->submission) }}
+                                        </div>
+
+                                        @if ($submission->github_commit_link)
+                                            <a class="btn btn-outline-dark btn-sm mb-2" href="{{ $submission->github_commit_link }}" target="_blank">
+                                                <span class="fa fa-github"></span> GitHub
+                                            </a>
+                                        @endif
+
+                                        <div class="small text-muted">
+                                            <strong>Posted: </strong>
+                                            {{ $submission->created_at->format('d M Y') }},
+                                            {{ $submission->created_at->diffForHumans() }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+                        <div class="mb-5">
+                            {{ $assignment->submissions()->simplePaginate(12)->links() }}
                         </div>
                     @endif
                 </div>
