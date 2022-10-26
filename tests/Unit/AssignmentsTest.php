af88d4b72c00b02cdc6ebd56faec2075be2896b3
Hamza Mughal <hamza@prodesquare.com>
2022-10-27 03:44:09 +0500

    commented out `test_student_can_mark_assignment_as_done` test

--- tests/Unit/AssignmentsTest.php
+++ tests/Unit/AssignmentsTest.php
@@ -30,15 +30,16 @@ class AssignmentsTest extends TestCase
         $response->assertRedirect('/group/view/project/1/milestones/view/1');
     }
 
-    public function test_student_can_mark_assignment_as_done()
-    {
-        $this->actingAs(Student::first(), 'student');
+    // teacher can mark assignment as done
+    // public function test_student_can_mark_assignment_as_done()
+    // {
+    //     $this->actingAs(Student::first(), 'student');
 
-        $response = $this->put('/group/view/project/1/milestones/1/assignments/1/mark-as-completed', [
-            'github_commit_link' => 'https://github.com/ProDeSquare/final-year-project/commit/079d736d8d974a91e6bd8d10f1662c4967dad3f6',
-            'is_completed' => 1,
-        ]);
+    //     $response = $this->put('/group/view/project/1/milestones/1/assignments/1/mark-as-completed', [
+    //         'github_commit_link' => 'https://github.com/ProDeSquare/final-year-project/commit/079d736d8d974a91e6bd8d10f1662c4967dad3f6',
+    //         'is_completed' => 1,
+    //     ]);
 
-        $response->assertRedirect('/group/view/project/1/milestones/1/assignments/view/1');
-    }
+    //     $response->assertRedirect('/group/view/project/1/milestones/1/assignments/view/1');
+    // }
 }
