c22a9d3e2e75a0c0057fda7d07f81f103f7591fb
Hamza Mughal <hamza@prodesquare.com>
2022-11-01 00:49:08 +0500

    storing appeals

--- app/Http/Controllers/Rejections/AppealController.php
+++ app/Http/Controllers/Rejections/AppealController.php
@@ -7,6 +7,7 @@ use App\Models\Project;
 use App\Models\Rejection;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AppealController extends Controller
 {
@@ -15,8 +16,30 @@ class AppealController extends Controller
         $this->middleware('auth:admin,mod,teacher,student');
     }
 
+    public function add(Request $request, Project $project, Rejection $rejection)
+    {
+        $project->rejections()->findOrFail($rejection->id);
+
+        if ($project->status === 1) abort(403);
+
+        if (!$this->projectBelongsToUser($project, $rejection)) abort(404);
+
+        $appeal = $rejection->appeals()->create([
+            'user_id' => Auth::guard()->id(),
+            'guard' => Auth::guard()->user()->getGuardType(),
+
+            ...$request->validate([
+                'text' => 'required',
+            ]),
+        ]);
+
+        return redirect(route('view.appeals', [$project, $rejection]) . "#appeal-" . $appeal->id);
+    }
+
     public function show(Project $project, Rejection $rejection)
     {
+        $project->rejections()->findOrFail($rejection->id);
+
         $group = Group::find($project->group_id);
 
         return view('projects.appeals')->with([
@@ -25,5 +48,12 @@ class AppealController extends Controller
             'rejection' => $rejection,
             'appeals' => $rejection->appeals()->paginate(20),
         ]);
+    }
+
+    protected function projectBelongsToUser(Project $project, Rejection $rejection)
+    {
+        return Auth::guard()->user()->getGuardType() === 'student'
+            ? $project->group_id === Auth::guard()->user()->getGroupId()
+            : $rejection->teacher_id === Auth::guard()->id();
     }
 }
