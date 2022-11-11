3b48c7f88a5de54bd6991bf5633b985f195c5430
Hamza Mughal <hamza@prodesquare.com>
2022-11-11 05:23:06 +0500

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
 
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
+
     <script src="{{ asset('assets/js/require.min.js') }}"></script>
     <script>
         requirejs.config({
