87d0c66e2a04a5fafe36e03c6c59c3c180965f37
Hamza Mughal <hamza@prodesquare.com>
2022-11-13 05:27:12 +0500

    textarea(s) resize with the text

--- public/assets/js/resize-textarea.js
+++ public/assets/js/resize-textarea.js
@@ -1,6 +1,6 @@
 // adding auto resize functionality to all textareas in the app
 document.querySelectorAll("textarea").forEach(el => {
-    let offset = el.offsetHeight - el.clientHeight
+    const offset = el.offsetHeight - el.clientHeight
     el.addEventListener("input", e => {
         e.target.style.height = "auto"
         e.target.style.height = `${e.target.scrollHeight + offset}px`
