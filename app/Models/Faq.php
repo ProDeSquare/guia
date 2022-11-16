577d77ab565c63e670070981fafcc56b7f03d100
Hamza Mughal <hamza@prodesquare.com>
2022-11-17 03:46:28 +0500

    searchable method in `Faq::class()`

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Searchable\SearchResult;
+use Spatie\Searchable\Searchable;
 
-class Faq extends Model
+class Faq extends Model implements Searchable
 {
     use HasFactory;
 
@@ -13,4 +15,15 @@ class Faq extends Model
         'question',
         'answer',
     ];
+
+    public function getSearchResult(): SearchResult
+    {
+        $url = route('faq.view', $this->id);
+
+        return new SearchResult(
+            $this,
+            $this->title,
+            $url,
+        );
+    }
 }
