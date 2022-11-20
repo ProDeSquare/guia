4db6d83f0b19ec1441ce98afc73c3d5524affceb
Hamza Mughal <hamza@prodesquare.com>
2022-11-21 02:24:01 +0500

    admin profile card updated avatar

--- resources/views/partials/admin/profile-card.blade.php
+++ resources/views/partials/admin/profile-card.blade.php
@@ -2,7 +2,13 @@
     <div class="card-header"></div>
 
     <div class="card-body text-center">
-        <div class="profile-image-container" style="background-image: url({{ $admin->avatar() }})"></div>
+        <div class="profile-image-container">
+            <img
+                src="{{ $admin->avatar() }}"
+                alt="{{ $admin->name }}'s avatar"
+                onerror="this.onerror=null;this.src='https://www.gravatar.com/avatar/{{ md5($admin->email) }}?d=mm'"
+            />
+        </div>
 
         <h3 class="mb-3">{{ $admin->name }}</h3>
         <p class="mb-4">
