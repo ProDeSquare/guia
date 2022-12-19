92c1b2d2ed68ee27f54de5fb46412c8c4349ccb7
Hamza Mughal <hamza@prodesquare.com>
2022-12-20 02:04:30 +0500

    removed documentation link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,9 +4,13 @@
             <div class="col-auto ml-lg-auto">
                 <div class="row align-items-center">
                     <div class="col-auto">
-                        <ul class="list-inline list-inline-dots mb-0">
-                            <li class="list-inline-item"><a href="#">Documentation</a></li>
-                            <li class="list-inline-item"><a href="{{ route('faqs') }}">FAQ</a></li>
+                        <ul class="list-inline list-inline-dots mb-0" id="guia-footer-links">
+                            <li class="list-inline-item">
+                                <a href="https://github.com/ProDeSquare/guia">
+                                    <i class="fe fe-github mr-1"></i> Source
+                                </a>
+                            </li>
+                            <li class="list-inline-item"><a href="{{ route('faqs') }}">FAQs</a></li>
                         </ul>
                     </div>
                 </div>
