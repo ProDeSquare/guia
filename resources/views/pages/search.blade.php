53771ed5edb266ced7bd96819465063f57e597c1
Hamza Mughal <hamza@prodesquare.com>
2023-02-01 03:20:37 +0500

    search results page layout set

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -19,23 +19,36 @@
         </div>
 
         <div class="page-body">
-            @if (strlen(app('request')->input('q')) < 3)
-                There were no search results.
-            @else
-                @include('partials.search-results')
-            @endif
+            <div class="row">
+                <div class="col-lg-8">
+                    @if (strlen(app('request')->input('q')) < 3)
+                        There were no search results.
+                    @else
+                        @include('partials.search-results')
+                    @endif
+                </div>
 
-            @if (Auth::guard()->user()->searchHistory()->count())
-                <h3 class="page-title mt-6">Search History.</h3>
-
-                <ul>
-                    @foreach (Auth::guard()->user()->searchHistory()->latest()->take(10)->get() as $searchQuery)
-                        <li>
-                            <a href="{{ route('search') . '?q=' . $searchQuery->query }}">{{ $searchQuery->query }}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            @endif
+                <div class="col-lg-4">
+                    @if (Auth::guard()->user()->searchHistory()->count())
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    Search History ({{ Auth::guard()->user()->searchHistory()->count() }})
+                                </h3>
+                            </div>
+                            <div class="card-body">
+                                <ul>
+                                    @foreach (Auth::guard()->user()->searchHistory()->latest()->take(10)->get() as $searchQuery)
+                                        <li>
+                                            <a href="{{ route('search') . '?q=' . $searchQuery->query }}">{{ $searchQuery->query }}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
 @endsection
