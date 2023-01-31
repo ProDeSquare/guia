af0ac5fbac62e5d52744b11d8c7f5cff42b4072d
Hamza Mughal <hamza@prodesquare.com>
2023-02-01 04:48:28 +0500

    students and teachers search result cards

--- resources/views/partials/search-results.blade.php
+++ resources/views/partials/search-results.blade.php
@@ -3,13 +3,36 @@
 </p>
 
 @foreach ($results->groupByType() as $type => $modelSearchResults)
-    <h4>{{ ucwords($type) }}</h4>
+    @if (in_array($type, ['students', 'teachers']))
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">{{ ucwords($type) }}</h3>
+            </div>
 
-    @foreach ($modelSearchResults as $result)
-        <ul>
-            <li>
-                <a href="{{ $result->url }}">{{ $result->title }}</a>
-            </li>
-        </ul>
-    @endforeach
+            <div class="card-body o-auto" style="max-height: 15rem">
+                <ul class="list-unstyled list-separated">
+                    @foreach ($modelSearchResults as $result)
+                        <li class="list-separated-item">
+                            <div class="row align-items-center">
+                                <div class="col-auto">
+                                    <span
+                                        class="avatar avatar-md d-block"
+                                        style="background-image: url({{ $result->searchable->avatar() }}), url(https://www.gravatar.com/avatar/{{ md5($result->searchable->email) }}?d=mm)"
+                                    ></span>
+                                </div>
+                                <div class="col">
+                                    <div>
+                                        <a href="{{ route(rtrim($type, 's') . '.profile', $result->searchable->id) }}" class="text-inherit">{{ $result->searchable->name }}</a>
+                                    </div>
+                                    <small class="d-block item-except text-sm text-muted h-1x">
+                                        {{ $result->searchable->email }}
+                                    </small>
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    @endif
 @endforeach
